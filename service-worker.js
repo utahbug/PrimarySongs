22ffaf62e2e4a3acdee27012799e984ef52bd3da
utahbug <ksrid@icloud.com>
2026-07-27 13:52:08 -0600

    Angle tuning fork horizontally

--- service-worker.js
+++ service-worker.js
@@ -1,10 +1,10 @@
-const CACHE_NAME = "primary-music-helper-shell-v240";
+const CACHE_NAME = "primary-music-helper-shell-v241";
 const APP_SHELL = [
   "./",
   "./index.html",
-  "./styles.css?v=primary-2026-77",
-  "./lyrics-cards.js?v=primary-2026-77",
-  "./script.js?v=primary-2026-77",
+  "./styles.css?v=primary-2026-78",
+  "./lyrics-cards.js?v=primary-2026-78",
+  "./script.js?v=primary-2026-78",
   "./library.json",
   "./manifest.json",
   "./favicon.ico",
