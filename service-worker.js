c051bf3b12f630cc697f56965d3d747909f580f5
utahbug <ksrid@icloud.com>
2026-07-26 20:42:36 -0600

    Constrain Files search width

--- service-worker.js
+++ service-worker.js
@@ -1,10 +1,10 @@
-const CACHE_NAME = "primary-music-helper-shell-v197";
+const CACHE_NAME = "primary-music-helper-shell-v198";
 const APP_SHELL = [
   "./",
   "./index.html",
-  "./styles.css?v=primary-2026-34",
-  "./lyrics-cards.js?v=primary-2026-34",
-  "./script.js?v=primary-2026-34",
+  "./styles.css?v=primary-2026-35",
+  "./lyrics-cards.js?v=primary-2026-35",
+  "./script.js?v=primary-2026-35",
   "./library.json",
   "./manifest.json",
   "./favicon.ico",
