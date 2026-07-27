fed25e578a42a7b52f34ea8249939d8c8d1ea373
utahbug <ksrid@icloud.com>
2026-07-26 22:13:24 -0600

    Shift banner blue gradient right

--- service-worker.js
+++ service-worker.js
@@ -1,10 +1,10 @@
-const CACHE_NAME = "primary-music-helper-shell-v213";
+const CACHE_NAME = "primary-music-helper-shell-v214";
 const APP_SHELL = [
   "./",
   "./index.html",
-  "./styles.css?v=primary-2026-50",
-  "./lyrics-cards.js?v=primary-2026-50",
-  "./script.js?v=primary-2026-50",
+  "./styles.css?v=primary-2026-51",
+  "./lyrics-cards.js?v=primary-2026-51",
+  "./script.js?v=primary-2026-51",
   "./library.json",
   "./manifest.json",
   "./favicon.ico",
