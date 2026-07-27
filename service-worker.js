88b92a3db53715a5af8ce4bcef6ee979e1118a0e
utahbug <ksrid@icloud.com>
2026-07-26 22:35:18 -0600

    Replace PDF guide bulb with Info

--- service-worker.js
+++ service-worker.js
@@ -1,10 +1,10 @@
-const CACHE_NAME = "primary-music-helper-shell-v216";
+const CACHE_NAME = "primary-music-helper-shell-v217";
 const APP_SHELL = [
   "./",
   "./index.html",
-  "./styles.css?v=primary-2026-53",
-  "./lyrics-cards.js?v=primary-2026-53",
-  "./script.js?v=primary-2026-53",
+  "./styles.css?v=primary-2026-54",
+  "./lyrics-cards.js?v=primary-2026-54",
+  "./script.js?v=primary-2026-54",
   "./library.json",
   "./manifest.json",
   "./favicon.ico",
