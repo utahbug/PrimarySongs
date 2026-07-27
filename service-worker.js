d3269b967a0b868e276423bf00505ddfd77a56ac
utahbug <ksrid@icloud.com>
2026-07-27 14:02:10 -0600

    Build About card and remove Lists accent rail

--- service-worker.js
+++ service-worker.js
@@ -1,10 +1,10 @@
-const CACHE_NAME = "primary-music-helper-shell-v242";
+const CACHE_NAME = "primary-music-helper-shell-v243";
 const APP_SHELL = [
   "./",
   "./index.html",
-  "./styles.css?v=primary-2026-79",
-  "./lyrics-cards.js?v=primary-2026-79",
-  "./script.js?v=primary-2026-79",
+  "./styles.css?v=primary-2026-80",
+  "./lyrics-cards.js?v=primary-2026-80",
+  "./script.js?v=primary-2026-80",
   "./library.json",
   "./manifest.json",
   "./favicon.ico",
