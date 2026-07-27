d69bd90d64a3cd18eafa3fed78fde8d515f9fa53
utahbug <ksrid@icloud.com>
2026-07-27 13:47:32 -0600

    Give Music Tools a dimensional color icon

--- service-worker.js
+++ service-worker.js
@@ -1,10 +1,10 @@
-const CACHE_NAME = "primary-music-helper-shell-v239";
+const CACHE_NAME = "primary-music-helper-shell-v240";
 const APP_SHELL = [
   "./",
   "./index.html",
-  "./styles.css?v=primary-2026-76",
-  "./lyrics-cards.js?v=primary-2026-76",
-  "./script.js?v=primary-2026-76",
+  "./styles.css?v=primary-2026-77",
+  "./lyrics-cards.js?v=primary-2026-77",
+  "./script.js?v=primary-2026-77",
   "./library.json",
   "./manifest.json",
   "./favicon.ico",
