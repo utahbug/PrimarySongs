a7fc33653a06e4b36aeb09f2235a5be0a19a4e96
utahbug <ksrid@icloud.com>
2026-07-26 22:44:51 -0600

    Simplify PDF tap-zone prompt

--- service-worker.js
+++ service-worker.js
@@ -1,10 +1,10 @@
-const CACHE_NAME = "primary-music-helper-shell-v218";
+const CACHE_NAME = "primary-music-helper-shell-v219";
 const APP_SHELL = [
   "./",
   "./index.html",
-  "./styles.css?v=primary-2026-55",
-  "./lyrics-cards.js?v=primary-2026-55",
-  "./script.js?v=primary-2026-55",
+  "./styles.css?v=primary-2026-56",
+  "./lyrics-cards.js?v=primary-2026-56",
+  "./script.js?v=primary-2026-56",
   "./library.json",
   "./manifest.json",
   "./favicon.ico",
