5f6bebf827fbbe03e3ceb2180f89fa2e8f350be0
utahbug <ksrid@icloud.com>
2026-07-27 13:56:23 -0600

    Replace Music Tools icon with silver tuning fork

--- service-worker.js
+++ service-worker.js
@@ -1,10 +1,10 @@
-const CACHE_NAME = "primary-music-helper-shell-v241";
+const CACHE_NAME = "primary-music-helper-shell-v242";
 const APP_SHELL = [
   "./",
   "./index.html",
-  "./styles.css?v=primary-2026-78",
-  "./lyrics-cards.js?v=primary-2026-78",
-  "./script.js?v=primary-2026-78",
+  "./styles.css?v=primary-2026-79",
+  "./lyrics-cards.js?v=primary-2026-79",
+  "./script.js?v=primary-2026-79",
   "./library.json",
   "./manifest.json",
   "./favicon.ico",
