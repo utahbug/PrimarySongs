c80304e205fc2828d92768ad54515c6ed18960d6
utahbug <ksrid@icloud.com>
2026-07-28 07:32:41 -0600

    Refine Help tap-zone diagram

--- service-worker.js
+++ service-worker.js
@@ -1,10 +1,10 @@
-const CACHE_NAME = "primary-music-helper-shell-v260";
+const CACHE_NAME = "primary-music-helper-shell-v261";
 const APP_SHELL = [
   "./",
   "./index.html",
-  "./styles.css?v=primary-2026-97",
-  "./lyrics-cards.js?v=primary-2026-97",
-  "./script.js?v=primary-2026-97",
+  "./styles.css?v=primary-2026-98",
+  "./lyrics-cards.js?v=primary-2026-98",
+  "./script.js?v=primary-2026-98",
   "./library.json",
   "./manifest.json",
   "./favicon.ico",
