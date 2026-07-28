f784fabc278849203e2fc706211bc6dfd3928c5c
utahbug <ksrid@icloud.com>
2026-07-28 08:02:44 -0600

    Move app refresh control into About

--- service-worker.js
+++ service-worker.js
@@ -1,10 +1,10 @@
-const CACHE_NAME = "primary-music-helper-shell-v263";
+const CACHE_NAME = "primary-music-helper-shell-v264";
 const APP_SHELL = [
   "./",
   "./index.html",
-  "./styles.css?v=primary-2026-100",
+  "./styles.css?v=primary-2026-101",
   "./lyrics-cards.js?v=primary-2026-99",
-  "./script.js?v=primary-2026-100",
+  "./script.js?v=primary-2026-101",
   "./library.json",
   "./manifest.json",
   "./favicon.ico",
