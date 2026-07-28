eac4722cfc46df340ae6f9aff6b40ab3a22bfca0
utahbug <ksrid@icloud.com>
2026-07-27 19:19:25 -0600

    Refine About description

--- service-worker.js
+++ service-worker.js
@@ -1,10 +1,10 @@
-const CACHE_NAME = "primary-music-helper-shell-v249";
+const CACHE_NAME = "primary-music-helper-shell-v250";
 const APP_SHELL = [
   "./",
   "./index.html",
-  "./styles.css?v=primary-2026-86",
-  "./lyrics-cards.js?v=primary-2026-86",
-  "./script.js?v=primary-2026-86",
+  "./styles.css?v=primary-2026-87",
+  "./lyrics-cards.js?v=primary-2026-87",
+  "./script.js?v=primary-2026-87",
   "./library.json",
   "./manifest.json",
   "./favicon.ico",
