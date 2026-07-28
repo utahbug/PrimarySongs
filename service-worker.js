69ad1b1cbf256cfb3039dbd8b65703b5f50ec99a
utahbug <ksrid@icloud.com>
2026-07-28 09:01:59 -0600

    Label PDF tap-zone control clearly

--- service-worker.js
+++ service-worker.js
@@ -1,10 +1,10 @@
-const CACHE_NAME = "primary-music-helper-shell-v267";
+const CACHE_NAME = "primary-music-helper-shell-v268";
 const APP_SHELL = [
   "./",
   "./index.html",
-  "./styles.css?v=primary-2026-104",
+  "./styles.css?v=primary-2026-105",
   "./lyrics-cards.js?v=primary-2026-100",
-  "./script.js?v=primary-2026-104",
+  "./script.js?v=primary-2026-105",
   "./library.json",
   "./manifest.json",
   "./favicon.ico",
