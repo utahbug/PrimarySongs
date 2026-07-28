d06dde335ca3230685733b6caea1cc50119b4bc3
utahbug <ksrid@icloud.com>
2026-07-28 09:16:31 -0600

    Center banner information icon

--- service-worker.js
+++ service-worker.js
@@ -1,10 +1,10 @@
-const CACHE_NAME = "primary-music-helper-shell-v269";
+const CACHE_NAME = "primary-music-helper-shell-v270";
 const APP_SHELL = [
   "./",
   "./index.html",
-  "./styles.css?v=primary-2026-106",
+  "./styles.css?v=primary-2026-107",
   "./lyrics-cards.js?v=primary-2026-100",
-  "./script.js?v=primary-2026-106",
+  "./script.js?v=primary-2026-107",
   "./library.json",
   "./manifest.json",
   "./favicon.ico",
