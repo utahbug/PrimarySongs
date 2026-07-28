52263588300c9ee00e55dd2fe5e445562d2620df
utahbug <ksrid@icloud.com>
2026-07-27 19:17:29 -0600

    Remove redundant Music Tools caption

--- service-worker.js
+++ service-worker.js
@@ -1,10 +1,10 @@
-const CACHE_NAME = "primary-music-helper-shell-v248";
+const CACHE_NAME = "primary-music-helper-shell-v249";
 const APP_SHELL = [
   "./",
   "./index.html",
-  "./styles.css?v=primary-2026-85",
-  "./lyrics-cards.js?v=primary-2026-85",
-  "./script.js?v=primary-2026-85",
+  "./styles.css?v=primary-2026-86",
+  "./lyrics-cards.js?v=primary-2026-86",
+  "./script.js?v=primary-2026-86",
   "./library.json",
   "./manifest.json",
   "./favicon.ico",
