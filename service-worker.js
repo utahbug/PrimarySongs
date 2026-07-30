fc8ad77d699f0651ba6a94443fd8bc1496f7b735
utahbug <ksrid@icloud.com>
2026-07-29 20:56:46 -0600

    Align Chord Guide heading

--- service-worker.js
+++ service-worker.js
@@ -1,8 +1,8 @@
-const CACHE_NAME = "primary-music-helper-shell-v343";
+const CACHE_NAME = "primary-music-helper-shell-v344";
 const APP_SHELL = [
   "./",
   "./index.html",
-  "./styles.css?v=primary-2026-153",
+  "./styles.css?v=primary-2026-154",
   "./lyrics-cards.js?v=primary-2026-100",
   "./script.js?v=primary-2026-152",
   "./library.json",
