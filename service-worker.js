54ac7f9cf9754bcb4a46002d6843a374cde01c0c
utahbug <ksrid@icloud.com>
2026-08-01 15:21:45 -0600

    Show list controls only when selected

--- service-worker.js
+++ service-worker.js
@@ -1,8 +1,8 @@
-const CACHE_NAME = "primary-music-helper-shell-v458";
+const CACHE_NAME = "primary-music-helper-shell-v459";
 const APP_SHELL = [
   "./",
   "./index.html",
-  "./styles.css?v=primary-2026-232",
+  "./styles.css?v=primary-2026-233",
   "./lyrics-cards.js?v=primary-2026-101",
   "./script.js?v=primary-2026-230",
   "./library.json",
