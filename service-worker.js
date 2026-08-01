ba8fcdf3e863358150f81888468ff349e296f467
utahbug <ksrid@icloud.com>
2026-08-01 10:54:37 -0600

    Move Fun Stuff activities above play area

--- service-worker.js
+++ service-worker.js
@@ -1,8 +1,8 @@
-const CACHE_NAME = "primary-music-helper-shell-v443";
+const CACHE_NAME = "primary-music-helper-shell-v444";
 const APP_SHELL = [
   "./",
   "./index.html",
-  "./styles.css?v=primary-2026-223",
+  "./styles.css?v=primary-2026-224",
   "./lyrics-cards.js?v=primary-2026-101",
   "./script.js?v=primary-2026-222",
   "./library.json",
