f4b3da9925957a425380f2dfef515743daa71963
utahbug <ksrid@icloud.com>
2026-07-31 22:45:19 -0600

    Add prototype notice and search restrictions

--- service-worker.js
+++ service-worker.js
@@ -1,8 +1,8 @@
-const CACHE_NAME = "primary-music-helper-shell-v419";
+const CACHE_NAME = "primary-music-helper-shell-v421";
 const APP_SHELL = [
   "./",
   "./index.html",
-  "./styles.css?v=primary-2026-205",
+  "./styles.css?v=primary-2026-207",
   "./lyrics-cards.js?v=primary-2026-101",
   "./script.js?v=primary-2026-206",
   "./library.json",
