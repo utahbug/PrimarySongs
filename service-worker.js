ad5bd3faefb26cee58aa3a3d899a62b57196fc55
utahbug <ksrid@icloud.com>
2026-08-01 18:46:31 -0600

    Use combined PDF tools on all screens

--- service-worker.js
+++ service-worker.js
@@ -1,8 +1,8 @@
-const CACHE_NAME = "primary-music-helper-shell-v489";
+const CACHE_NAME = "primary-music-helper-shell-v490";
 const APP_SHELL = [
   "./",
   "./index.html",
-  "./styles.css?v=primary-2026-250",
+  "./styles.css?v=primary-2026-251",
   "./lyrics-cards.js?v=primary-2026-101",
   "./script.js?v=primary-2026-248",
   "./library.json",
